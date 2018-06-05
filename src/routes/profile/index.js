b1c4fe4edbbfd3619b16814ade9e0a20e22df171
Maarten Bicknese <maarten.bicknese@gmail.com>
2018-06-05 11:24:13 +0200

    Change linting to Standard style

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,47 +1,52 @@
-import { h, Component } from 'preact';
-import style from './style';
+import { Component } from 'preact'
+import style from './style'
 
 export default class Profile extends Component {
-	state = {
-		time: Date.now(),
-		count: 10
-	};
+  state = {
+    time: Date.now(),
+    count: 10
+  };
 
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
+  constructor () {
+    super()
+    this.updateTime = this.updateTime.bind(this)
+    this.increment = this.increment.bind(this)
+  }
 
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
+  // update the current time
+  updateTime () {
+    this.setState({ time: Date.now() })
+  };
 
-	// gets called when this route is navigated to
-	componentDidMount() {
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
+  increment () {
+    this.setState({ count: this.state.count + 1 })
+  };
 
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
+  // gets called when this route is navigated to
+  componentDidMount () {
+    // start a timer for the clock:
+    this.timer = setInterval(this.updateTime, 1000)
+  }
 
-	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
-		return (
-			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
+  // gets called just before navigating away from the route
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
 
-				<div>Current time: {new Date(time).toLocaleString()}</div>
+  // Note: `user` comes from the URL, courtesy of our router
+  render ({ user }, { time, count }) {
+    return (
+      <div class={style.profile}>
+        <h1>Profile: {user}</h1>
+        <p>This is the user profile for a user named {user}.</p>
 
-				<p>
-					<button onClick={this.increment}>Click Me</button>
-					{' '}
-					Clicked {count} times.
-				</p>
-			</div>
-		);
-	}
+        <div>Current time: {new Date(time).toLocaleString()}</div>
+
+        <p>
+          <button onClick={this.increment}>Click Me</button> Clicked {count}{' '}
+          times.
+        </p>
+      </div>
+    )
+  }
 }
